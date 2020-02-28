a565b9776f38f52e44b9dfaa67f8bff7a6752e19
ARPetrenko <ms.suslikmur@mail.ru>
2020-02-28 10:22:59 +0300

    переделала css

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 let targsAnimation01 = anime({
   targets: '.first',
-  translateX: 500,
-  translateY: 200,
-  rotate: ['-15deg', '375deg'],
-  easing: 'steps(5)',
+  rotate: ['-15deg', '-360deg'],
   direction: 'alternate',
-  borderRadius: ['0%', '50%'],
-  height: [343, 31],
   autoplay: false,
 })
 let targsBtn01 = document.getElementById('frst')
@@ -14,9 +9,7 @@ targsBtn01.onmouseover = targsAnimation01.play
 
 let targsAnimation02 = anime({
   targets: '.second',
-  translateX: 300,
-  rotate: ['-15deg', '-375deg'],
-  easing: 'steps(10)',
+  rotate: ['-15deg', '360deg'],
   direction: 'alternate',
   autoplay: false,
 })
@@ -25,10 +18,7 @@ targsBtn02.onmouseover = targsAnimation02.play
 
 let targsAnimation03 = anime({
   targets: '.third',
-  rotate: ['-15deg', '720deg'],
-  easing: 'steps(10)',
-  translateY: -400,
-  height: '100vw',
+  rotate: ['-15deg', '-360deg'],
   direction: 'alternate',
   autoplay: false,
 })
@@ -36,14 +26,71 @@ let targsBtn03 = document.getElementById('thrd')
 targsBtn03.onmouseover = targsAnimation03.play
 
 
-let targsAnimation03 = anime({
-  targets: '.third',
-  rotate: ['-15deg', '720deg'],
-  easing: 'steps(10)',
-  translateY: -400,
-  height: '100vw',
+let targsAnimation04 = anime({
+  targets: '.fourth',
+  rotate: ['34deg', '360deg'],
   direction: 'alternate',
   autoplay: false,
 })
-let targsBtn03 = document.getElementById('thrd')
-targsBtn03.onmouseover = targsAnimation03.play
+let targsBtn04 = document.getElementById('frth')
+targsBtn04.onmouseover = targsAnimation04.play
+
+
+let targsAnimation05 = anime({
+  targets: '.fifth',
+  rotate: ['75deg', '-360deg'],
+  direction: 'alternate',
+  autoplay: false,
+})
+let targsBtn05 = document.getElementById('ffth')
+targsBtn05.onmouseover = targsAnimation05.play
+
+
+let targsAnimation06 = anime({
+  targets: '.sixth',
+  rotate: ['75deg', '360deg'],
+  direction: 'alternate',
+  autoplay: false,
+})
+let targsBtn06 = document.getElementById('sxth')
+targsBtn06.onmouseover = targsAnimation06.play
+
+
+let targsAnimation07 = anime({
+  targets: '.seventh',
+  rotate: ['75deg', '-360deg'],
+  direction: 'alternate',
+  autoplay: false,
+})
+let targsBtn07 = document.getElementById('svnth')
+targsBtn07.onmouseover = targsAnimation07.play
+
+
+let targsAnimation08 = anime({
+  targets: '.eight',
+  rotate: ['-15deg', '360deg'],
+  direction: 'alternate',
+  autoplay: false,
+})
+let targsBtn08 = document.getElementById('ght')
+targsBtn08.onmouseover = targsAnimation08.play
+
+
+let targsAnimation09 = anime({
+  targets: '.nine',
+  rotate: ['95deg', '-360deg'],
+  direction: 'alternate',
+  autoplay: false,
+})
+let targsBtn09 = document.getElementById('nn')
+targsBtn09.onmouseover = targsAnimation09.play
+
+
+let targsAnimation10 = anime({
+  targets: '.ten',
+  rotate: ['-15deg', '450deg'],
+  direction: 'alternate',
+  autoplay: false,
+})
+let targsBtn10 = document.getElementById('tn')
+targsBtn10.onmouseover = targsAnimation10.play
